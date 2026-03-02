356c6b789c30460af7366dcc4267ca06a2ebb353
DaveLee <62046018+DaveLee-fun@users.noreply.github.com>
2026-03-02 18:47:17 +0900

    Initial commit: AgileClaw source

---
+++ tools/__init__.py
@@ -0,0 +1,13 @@
+from tools import shell, files, web, browser
+
+ALL_TOOL_DEFINITIONS = [
+    shell.TOOL_DEFINITION,
+    files.TOOL_READ,
+    files.TOOL_WRITE,
+    web.TOOL_DEFINITION,
+    browser.TOOL_OPEN,
+    browser.TOOL_SCREENSHOT,
+    browser.TOOL_CLICK,
+    browser.TOOL_TYPE,
+    browser.TOOL_GET_TEXT,
+]
